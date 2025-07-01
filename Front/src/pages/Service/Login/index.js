6a79563347a52bace1b06f38bd89cb3889e16521
pppbin__ <hyunbin0525@naver.com>
2025-07-01 21:28:19 +0900

    feat: login logic files

--- Front/src/pages/Service/Login/index.js
+++ Front/src/pages/Service/Login/index.js
@@ -1,5 +1,12 @@
-const LoginPage = () => {
+import React from 'react';
+import LoginForm from '../../components/LoginForm';
 
+const LoginPage = () => {
+  return(
+    <div>
+      <LoginForm />
+    </div>
+  )
 }
 
 export default LoginPage;
