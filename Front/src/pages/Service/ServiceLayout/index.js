724d6b4e0ef97d93438241a4b8a6593ce6ae322d
pppbin__ <hyunbin0525@naver.com>
2025-07-06 22:04:43 +0900

    로그인 연결을 위한 임시조치

--- Front/src/pages/Service/ServiceLayout/index.js
+++ Front/src/pages/Service/ServiceLayout/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
+import './index.css';
 
 const ServiceLayout = () => {
   return ( 
-    <div>
-      <Sidebar />
-      <Outlet />
+    <div className='serviceLayoutContainer'>
+      <div>
+        <Sidebar />
+      </div>
+      <div>
+        <Outlet />
+      </div>
+
+
     </div>
   )
 }
