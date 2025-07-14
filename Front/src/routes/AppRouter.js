9dac3a5609fcf61ab3680b75f5450c3b1e708f86
wannahappyaroundme <bu5119@hanyang.ac.kr>
2025-07-14 17:41:36 +0900

    Create Customer Support Modal in Sidebar

--- Front/src/routes/AppRouter.js
+++ Front/src/routes/AppRouter.js
@@ -5,33 +5,32 @@ import * as ServicePages from '../pages/Service';
 import * as AuthPages from '../pages/Auth';
 import * as CommonPages from '../pages/Common';
 
-
 export default function AppRouter() {
-  return (
-    <Routes>
-      {/* Landing Pages */}
-      <Route path="/" element={<LandingPages.LandingLayout />} />
-      <Route path="/team" element={<LandingPages.TeamPage />} />
+    return (
+        <Routes>
+            {/* Landing Pages */}
+            <Route path="/" element={<LandingPages.LandingLayout />} />
+            <Route path="/team" element={<LandingPages.TeamPage />} />
 
-      {/* Service Pages */}
-      <Route path="/service" element={<ServicePages.ServiceLayout />}>
-        <Route index element={<ServicePages.ServiceHome />} />
-        <Route path="clip" element={<ServicePages.ClipPage />} />
-        <Route path="data" element={<ServicePages.DataPage />} />
-        <Route path='team' element={<ServicePages.TeamPage/> } />
-        <Route path='support' element={<ServicePages.SupportPage/> } />
-        <Route path='faq' element={<ServicePages.FaqPage/> } />
-      </Route>
+            {/* Service Pages */}
+            <Route path="/service" element={<ServicePages.ServiceLayout />}>
+                <Route index element={<ServicePages.ServiceHome />} />
+                <Route path="clip" element={<ServicePages.ClipPage />} />
+                <Route path="data" element={<ServicePages.DataPage />} />
+                <Route path="team" element={<ServicePages.TeamPage />} />
+                <Route path="support" element={<ServicePages.SupportPage />} />
+                <Route path="faq" element={<ServicePages.FaqPage />} />
+            </Route>
 
-      {/* Auth Pages*/}
-      <Route path='auth' element={<AuthPages.AuthLayout />}>
-        <Route index element={<AuthPages.LoginPage />} />           
-        <Route path="signup" element={<AuthPages.SignupPage />} />
-        <Route path="find" element={<AuthPages.FindPage />} /> 
-        <Route path='verify-email' element={<AuthPages.VerifyEmailPage /> } />
-      </Route>
-      {/* 404 Not Found */}
-      <Route path="*" element={<CommonPages.NotFoundPage />} />
-    </Routes>
-  );
+            {/* Auth Pages*/}
+            <Route path="auth" element={<AuthPages.AuthLayout />}>
+                <Route index element={<AuthPages.LoginPage />} />
+                <Route path="signup" element={<AuthPages.SignupPage />} />
+                <Route path="find" element={<AuthPages.FindPage />} />
+                <Route path="verify-email" element={<AuthPages.VerifyEmailPage />} />
+            </Route>
+            {/* 404 Not Found */}
+            <Route path="*" element={<CommonPages.NotFoundPage />} />
+        </Routes>
+    );
 }
