23c44f8bb167bf2f65815352705bf12f98bbe650
pppbin__ <hyunbin0525@naver.com>
2025-07-17 16:46:16 +0900

    add seviceHomeHeader

--- Front/src/pages/Service/Home/index.js
+++ Front/src/pages/Service/Home/index.js
@@ -1,9 +1,21 @@
-
+import React from 'react';
+import './index.css';
 
 const ServiceHome = () => {
+  const HEADERTEXTS=['Date', 'Game Score', 'Detail', 'Report'];
+  
   return(
     <div className='serviceHomeContainer'>
-      <div>HI</div>
+      <div className='serviceHomeHeader'>
+        <div className='headerTextsBox'>
+          {HEADERTEXTS.map((t)=>(
+            <div className='headerTexts'>
+              {t}
+            </div>
+          ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
