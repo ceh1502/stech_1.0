5fc574a925c1d9303b095dc680e3741d4e6fcc83
Ken <kenlee2002@outlook.com>
2025-07-27 19:51:54 +0900

    fix: Video route

--- Back/index.js
+++ Back/index.js
@@ -21,7 +21,8 @@ const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 4000;
 
-app.use(express.json());
+app.use(express.json({ limit: '150mb' }));
+app.use(express.urlencoded({ limit: '150mb', extended: true }));
 
 // Swagger UI 라우트 추가 ← 여기에 추가!
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
