80760a25f1ca5e8c56f4eeebe5efcfd0a95e8758
yves <hyunbin0525@naver.com>
2025-08-23 08:12:04 +0900

    guest페이지로

--- Front/src/pages/Service/Guest/GuestStat/GuestLeagueTeam/index.js
+++ Front/src/pages/Service/Guest/GuestStat/GuestLeagueTeam/index.js
@@ -1,9 +1,11 @@
-
+import StatTeam from '../../../../../components/Stat/StatTeam';
+import { TEAMS } from '../../../../../data/TEAMS';
 
 const GuestLeagueTeamPage = () => {
     return (
         <div>
-           
+          {/* 건아 여기에 팀*/}
+           <StatTeam teams={TEAMS} />
         </div>
     );
 }
