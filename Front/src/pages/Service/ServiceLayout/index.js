f11ef78181cb6f257d6eabe327f8547476b416db
yves <hyunbin0525@naver.com>
2025-09-02 19:46:12 +0900

    Revert "서비스 레이아웃 재수정"
    This reverts commit c5280825879c30759a15c8f61eabae1ea25c17d4.

--- Front/src/pages/Service/ServiceLayout/index.js
+++ Front/src/pages/Service/ServiceLayout/index.js
@@ -1,39 +1,35 @@
 // src/pages/Service/ServiceLayout/index.js
-import React, { useState } from 'react';
-import { useLocation, useNavigate, Outlet, useMatch } from 'react-router-dom';
-import ServiceSidebar from './ServiceSidebar';
-import SupportModal from '../../../components/SupportModal';
-import UploadVideoModal from '../../../components/UploadVideoModal.jsx';
-import './index.css';
+import React, { useState } from "react";
+import { useLocation, useNavigate, Outlet } from "react-router-dom";
+import ServiceSidebar from "./ServiceSidebar";
+import SupportModal from "../../../components/SupportModal";
+import UploadVideoModal from "../../../components/UploadVideoModal.jsx";
+import "./index.css";
 
 const ServiceLayout = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const [showUpload, setShowUpload] = useState(false);
-  const isVideo = !!useMatch('/service/video/*');
 
   return (
     <>
-      {isVideo ? (
-        <div className="flex-1">
-          <Outlet/>
-        </div>
-      ) : (
-        <div className="serviceLayoutContainer">
-          <ServiceSidebar className="serviceSidebar" />
-          <main className="flex-1">
-            <Outlet />
-          </main>
-        </div>
-      )}
+      <div className="serviceLayoutContainer">
+        <ServiceSidebar className="serviceSidebar" />
+
+        <main className="flex-1">
+
+          <Outlet />
+        </main>
+      </div>
+
       <UploadVideoModal
         isOpen={showUpload}
         onClose={() => setShowUpload(false)}
-        onUploaded={() => console.log('upload ok')}
+        onUploaded={() => console.log("upload ok")}
       />
 
       {/* ---------- 3. 모달 ---------- */}
-      {location.pathname.startsWith('/service/support') && (
+      {location.pathname.startsWith("/service/support") && (
         <SupportModal onClose={() => navigate(-1)} />
       )}
     </>
